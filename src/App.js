6653297fa237a55ca4523b799b104d65c2a9fd4e
lhagva98 <lhaga98>
2021-04-08 12:58:30 +0800

    snake board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+function BoardBox(props) {
+  if(props.type == "head"){
+    return (
+      <div className="Box" />
+    )
+  }
+  return (
+    <div className="Box" />
+  )
+}
+
+function BoardRow(props) {
+  const { count } = props;
+
+  const getList = () => {
+    const list = [];
+    for(let i = 0; i < count; i ++) {
+      list.push(<BoardBox key={i}/>)
+    }
+    return list;
+  }
+
+  return (
+    <div className="row">
+      {getList()}
+    </div> 
+  )
+}
+
+function Score(props) {
+  return (
+    <div>{props.score}</div>
+  )
+}
+
+function Board(props) {
+  const { width, height, children} = props;
+  const renderRows = () => {
+    const list = [];
+    for(let i = 0; i < height; i ++) {
+      list.push(<BoardRow count={width} key={i}/>)
+    }
+    return list;
+  }
+
+  return (
+    <>
+      {renderRows()}
+      {children}
+    </>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>SNAKE</h1>
+        <Score score={20} />
+      </div>
+      <Board width={10} height={10}>
+        <h2>child1</h2>
+      </Board>
     </div>
   );
 }
